c9fb4deba6c42591332cf3118f2f746e2ea03db0
Sol <alvaro.figueroa3206@gmail.com>
2025-10-21 11:30:12 +0200

    Poniendo mapa a punto

--- src/Mapa.java
+++ src/Mapa.java
@@ -1,16 +1,24 @@
 import java.util.HashMap;
 
 public class Mapa {
-    public static final int ANCHO;
-    public static final int ALTO;
+    public static final int ANCHO = 15;
+    public static final int ALTO = 15;
     private final Posicion hormiguero;
     private final char[][] mapa;
-    public static final char VACIO;
-    public static final char HORMIGUERO;
+    public static final char VACIO = '.';
+    public static final char HORMIGUERO = 'H';
 
-    public Mapa(char[][] mapa, Posicion hormiguero) {
-        this.mapa = mapa;
-        this.hormiguero = hormiguero;
+    public Mapa() {
+        this.mapa = new char[this.ANCHO][this.ALTO];
+        this.hormiguero = new Posicion(this.ANCHO/2, this.ALTO/2);
+
+        for(int i = 0; i < this.ANCHO; i++){
+            for(int j = 0; j< this.ALTO; j++){
+                this.mapa[i][j] = this.VACIO;
+            }
+        }
+
+        mapa[this.hormiguero.getX()][this.hormiguero.getY()] = this.HORMIGUERO;
     }
 
     public Posicion getHormiguero() {
